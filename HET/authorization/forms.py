25b0131ec3b6336ea1b64524a42f910e61746d1b
sofi <@sofiiila>
2024-08-27 18:15:02 +0300

    стили формы регистрации, добавлены поя на русскрм языке и вспомогательный текст

--- HET/authorization/forms.py
+++ HET/authorization/forms.py
@@ -1,13 +1,40 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django import forms
 
 User = get_user_model()
 
-
 class CreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label='Пароль',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text='''
+                    Ваш пароль не должен быть слишком простым.
+                    Пароль должен содержать более 8 символов.
+                    Пароль не может состоять только из цифр.
+                    Пароль не должен повторять другие ваши пароли.
+                ''',
+    )
+    password2 = forms.CharField(
+        label='Повторите пароль',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text='Повторите пароль',
+    )
+
     class Meta(UserCreationForm.Meta):
-
         model = User
-
         fields = ('first_name', 'last_name', 'username', 'email')
-
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'username': 'Имя пользователя',
+            'email': 'Электронная почта',
+        }
+        help_texts = {
+            'first_name': 'Введите ваше имя.',
+            'last_name': 'Введите вашу фамилию.',
+            'username': 'Введите ваше имя пользователя.',
+            'email': 'Введите вашу электронную почту.',
+        }
