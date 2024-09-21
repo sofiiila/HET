4f59ffaad86692b7a24117567bb971037ee988f5
sofi <@sofiiila>
2024-09-21 14:11:19 +0300

    smth

--- HET/src/o.py
+++ HET/src/o.py
@@ -1,10 +1,12 @@
 from typing import Any
+from categories.models import Categories
+from categories.forms import CategoriesForm
 
 
 class CategoryEntyty:
     def get_categories_list(self):
-        return ['pussy', 'ASS']
-
+        all_categories = Categories.objects.all()
+        return all_categories
 
 
 class Controller:
@@ -12,11 +14,13 @@ class Controller:
     def __init__(self):
         self._category_entyty = CategoryEntyty()
 
-    def render_categories_page(self) -> dict[str, Any]:
+    def render_categories_page(self, request) -> dict[str, Any]:
         categories = self._category_entyty.get_categories_list()
         return {'test': 'test',
                 'categories': categories}
 
+    def add_category(self, request):
+        form = CategoriesForm(request.POST)
+        if form.is_valid():
+            form.save()
 
-name = Controller()
-print(name.render_categories_page())
